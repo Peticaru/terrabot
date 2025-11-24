dcb87a20482426e29592b13e4dfb3868ce2085c1
Peticaru <peti.informaticool@gmail.com>
2025-11-24 09:56:50 +0200

    added Weather conditions

--- src/main/java/main/WeatherConditions.java
+++ src/main/java/main/WeatherConditions.java
@@ -1,2 +1,16 @@
+package main;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class WeatherConditions {
+    public static String type;
+    public static double rainfall;
+    public static double windSpeed;
+    public static String Season;
+    public static boolean desertStorm;
+    public static int numberOfHikers;
 }
